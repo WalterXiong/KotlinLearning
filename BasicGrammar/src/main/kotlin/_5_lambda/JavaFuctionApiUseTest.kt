55b49108e8fff14dbcf1faec98f49393ccbff116
WalterXiong <xiongjun-ol@outlook.com>
2024-11-28 22:50:22 +0800

    基础语法模块更新

--- BasicGrammar/src/main/kotlin/_5_lambda/JavaFuctionApiUseTest.kt
+++ BasicGrammar/src/main/kotlin/_5_lambda/JavaFuctionApiUseTest.kt
@@ -1,4 +1,31 @@
 package _5_lambda
 
 class JavaFuctionApiUseTest {
+
+
+}
+
+fun main() {
+
+    Thread(object : Runnable {
+        override fun run() {
+            println("开启一个线程")
+        }
+    }).start()
+
+    Thread(Runnable {
+        println("开启一个线程")
+    }).start()
+
+    Thread({
+        println("开启一个线程")
+    }).start()
+
+    Thread() {
+        println("开启一个线程")
+    }.start()
+
+    Thread {
+        println("开启一个线程")
+    }.start()
 }
---
+++ BasicGrammar/src/main/kotlin/_6_NullPointerCheckIn/NullPointerCheckInTest.kt
@@ -0,0 +1,3 @@
+package _6_NullPointerCheckIn
+
+// 2.7
