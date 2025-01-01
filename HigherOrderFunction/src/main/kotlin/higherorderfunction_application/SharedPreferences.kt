9b14449b7809553801cd864c2b6e93f076f746e2
WalterXiong <xiongjun-ol@outlook.com>
2025-01-01 21:18:17 +0800

    新增--高阶函数应用

--- HigherOrderFunction/src/main/kotlin/higherorderfunction_application/SharedPreferences.kt
+++ HigherOrderFunction/src/main/kotlin/higherorderfunction_application/SharedPreferences.kt
@@ -3,10 +3,18 @@ package higherorderfunction_application
 
 class SharedPreferences {
 
+    fun edit(): SharedPreferences.Editor {
+        return this.edit()
+    }
+
+    inner class Editor {
+
+        fun apply() {}
+    }
 }
 
 fun SharedPreferences.open(block: SharedPreferences.Editor.() -> Unit) {
-    var edit = edit()
+    val edit = edit()
     edit.block()
     edit.apply()
 }
